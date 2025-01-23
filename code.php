5c3c2ded8c6bba456c76e45f4f6d0b1ebd0c53ab
Akash Singh <137589339+akashh7@users.noreply.github.com>
2025-01-23 14:59:29 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,33 +1,28 @@
+<!DOCTYPE html>
 <html>
-<body>
-<title> Missing guess parameter c0f762af</title>
-<h1> Welcome to my guessing game</h1>
+  <head>
+    <title>Missing guess parameter c0f762af</title>
+  </head>
+  <body>
+    <h1>Welcome to my guessing game</h1>
+    <?php
+      // Correct number set by the autograder
+      $correctnumber = 42; // This can be any number chosen by the autograder
 
-<?php
-$correctnumber=26; 
-if(isset($_GET['guess']))
-{
-  if (is_numeric($_GET['guess'])===FALSE)
- {
-echo "Your guess is not a number";
-}
-else if ($_GET['guess']<$correctnumber)
- {
-echo "Your guess is too low";
-}
-else if ($_GET['guess']>$correctnumber)
- {
-echo "Your guess is too high";
-}
-else if ($_GET['guess']==$correctnumber)
- {
-echo "Congratulations - You are right";
-}
-}
-else
-{
-echo "Missing guess parameter <br> Your guess is too short";
-}
-?>
-</body>
+      // Check if the 'guess' parameter exists in the GET request
+      if (!isset($_GET['guess'])) {
+        echo "Missing guess parameter <br> Your guess is too short";
+      } else if (strlen($_GET['guess']) < 1) {
+        echo "Your guess is too short";
+      } else if (!is_numeric($_GET['guess'])) {
+        echo "Your guess is not a number";
+      } else if ($_GET['guess'] < $correctnumber) {
+        echo "Your guess is too low";
+      } else if ($_GET['guess'] > $correctnumber) {
+        echo "Your guess is too high";
+      } else if ($_GET['guess'] == $correctnumber) {
+        echo "Congratulations - You are right";
+      }
+    ?>
+  </body>
 </html>
