4e4ddf03f4ba1120a474c6f3ef56ff54928494ed
Akash Singh <137589339+akashh7@users.noreply.github.com>
2025-01-28 12:12:43 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guessing Game</title>
+    <title>Guessing Game for Charles Severance c0f762af</title>
 </head>
 <body>
     <h1>Welcome to my guessing game</h1>
-    <form action="code.php" method="get">
-        <label for="guess">Enter your guess:</label>
-        <input type="text" name="guess" id="guess" placeholder="Enter a number" required>
-        <input type="submit" value="Submit Guess">
-    </form>
+    <p>Your guess is too low</p>
 </body>
 </html>
