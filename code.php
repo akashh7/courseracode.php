2a83338f123b740a09f359ee686ea13dca733ce9
Akash Singh <137589339+akashh7@users.noreply.github.com>
2025-01-28 12:06:38 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,1 +1,8 @@
-.
+<html style="user-select: auto;"><head style="user-select: auto;">
+<title style="user-select: auto;">Guessing Game for Charles Severance</title>
+</head>
+<body style="user-select: auto;">
+<h1 style="user-select: auto;">Welcome to my guessing game</h1>
+<p style="user-select: auto;">
+Your guess is too low</p>
+</body></html>
